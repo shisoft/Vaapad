eba1e794551efc2f26307be856e50870f3f1b24e
Jack Shi <shisoftgenius@gmail.com>
2017-11-05 03:07:02 +0800

    reset ignore

--- vaapad-derive/target/debug/deps/vaapad_derive-b5daa208960038b5.d
+++
@@ -1,5 +0,0 @@
-/home/shisoft/Documents/OSS Projects/Vaapad/vaapad-derive/target/debug/deps/libvaapad_derive-b5daa208960038b5.so: src/lib.rs
-
-/home/shisoft/Documents/OSS Projects/Vaapad/vaapad-derive/target/debug/deps/vaapad_derive-b5daa208960038b5.d: src/lib.rs
-
-src/lib.rs:
--- vaapad-derive/target/debug/libvaapad_derive.d
+++
@@ -1,1 +0,0 @@
-/home/shisoft/Documents/OSS\ Projects/Vaapad/vaapad-derive/target/debug/libvaapad_derive.so: /home/shisoft/Documents/OSS\ Projects/Vaapad/vaapad-derive/src/lib.rs
