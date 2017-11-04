e484cb3ea3d6a9bcbe4c1e944ae0cd49f3ad7148
Jack Shi <shisoftgenius@gmail.com>
2017-11-05 03:05:35 +0800

    scaffolding derive

--- vaapad-derive/src/lib.rs
+++ vaapad-derive/src/lib.rs
@@ -1,7 +1,29 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(Vaapad)]
+pub fn vaapad(input: TokenStream) -> TokenStream {
+    // Construct a string representation of the type definition
+    let s = input.to_string();
+    // Parse the string representation
+    let ast = syn::parse_derive_input(&s).unwrap();
+    // Build the impl
+    let gen = impl_vaapad(&ast);
+    // Return the generated impl
+    gen.parse().unwrap()
+}
+
+fn impl_vaapad(ast: &syn::DeriveInput) -> quote::Tokens {
+    let name = &ast.ident;
+    quote! {
+        impl HelloWorld for #name {
+            fn hello_world() {
+                println!("Hello, World! My name is {}", stringify!(#name));
+            }
+        }
     }
 }
---
+++ vaapad-derive/target/debug/libvaapad_derive.d
@@ -0,0 +1,1 @@
+/home/shisoft/Documents/OSS\ Projects/Vaapad/vaapad-derive/target/debug/libvaapad_derive.so: /home/shisoft/Documents/OSS\ Projects/Vaapad/vaapad-derive/src/lib.rs
